16cacf5ed292a8a19a4000f9b0fe359a7921b66d
Mateo <mateo.avalos3942@gmail.com>
2024-08-05 14:55:35 -0500

    Extended personal info and corrected css

--- Portfolio/src/components/Personal.tsx
+++ Portfolio/src/components/Personal.tsx
@@ -11,9 +11,11 @@ export const Personal = () => {
                     </Col>
                     <Col md={6}>
                         <h3>Get to know me!</h3>
-                        <p>I'm an undergraduate at EIA University, passionate about leveraging technology for healthcare advancements. 
-                            I have robust communication and problem-solving skills, with experience with programming general purpose 
-                            applications and microcontrollers. My most proficient programming languages are Java and Python (always learning!)..</p>
+                        <p>My name is Mateo Avalos. I'm an undergraduate at EIA University</p>
+                        <p>I'm profoundly passionate about leveraging technology for healthcare advancements.</p>
+                        <p>I have robust communication and problem-solving skills, with experience in programming general purpose 
+                            applications and microcontrollers. My most proficient programming languages are Java and Python (I'm currently learning web debeloping).</p>
+                        <p>If you're interested on knowing more about me, you have links to my GitHub, Instagram and Linkedin in the navitagion bar.</p>
                     </Col>
                 </Row>
             </Container>
