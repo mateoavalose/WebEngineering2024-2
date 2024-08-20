70797a06156361498a393be7011e397979b002a5
Mateo <mateo.avalos3942@gmail.com>
2024-08-20 08:45:00 -0500

    Added validation configuration

--- Nest-Example/src/main.ts
+++ Nest-Example/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
   const config = new DocumentBuilder()
   .setTitle('API Documentation')
   .setDescription('The API description')
