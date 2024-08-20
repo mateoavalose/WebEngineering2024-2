1dd2c8084081c005ba25cebea757f11532222b36
Mateo <mateo.avalos3942@gmail.com>
2024-08-20 08:41:53 -0500

    Configured swagger

--- Nest-Example/src/main.ts
+++ Nest-Example/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('API Documentation')
+  .setDescription('The API description')
+  .setVersion('0.01')
+  .addTag('Sushi')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
