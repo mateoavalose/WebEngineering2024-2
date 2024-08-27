3b20a24a747f0d9e9c0cef138b0519fa7322700d
Mateo <mateo.avalos3942@gmail.com>
2024-08-27 13:55:00 -0500

    Added basic ingredient entity

--- TypeORM-Nest/src/ingredients/entities/ingredient.entity.ts
+++ TypeORM-Nest/src/ingredients/entities/ingredient.entity.ts
@@ -1,1 +1,14 @@
-export class Ingredient {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Ingredient {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text',
+        {
+            unique: true
+        }
+    )
+    nombre: string;
+}
