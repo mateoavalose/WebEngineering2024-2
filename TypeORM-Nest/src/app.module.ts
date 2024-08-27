9f506e9fdf932368ef7c9823e95cf83d5ac647b5
Mateo <mateo.avalos3942@gmail.com>
2024-08-27 11:23:02 -0500

    Added config for TypeORM and env variables

--- TypeORM-Nest/src/app.module.ts
+++ TypeORM-Nest/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'; // TypeORM config
+import { ConfigModule } from '@nestjs/config'; // Access to .env file
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
