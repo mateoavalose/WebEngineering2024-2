1582c8a726d9eb4cf5ef11c70d746a0a8c408271
Mateo <mateo.avalos3942@gmail.com>
2024-08-27 14:02:39 -0500

    Added name restrictions

--- TypeORM-Nest/src/ingredients/dto/create-ingredient.dto.ts
+++ TypeORM-Nest/src/ingredients/dto/create-ingredient.dto.ts
@@ -1,1 +1,13 @@
-export class CreateIngredientDto {}
+import { IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateIngredientDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(3)
+    @ApiProperty({
+        example: 'Ingredient name',
+        description: 'The name of the ingredient'
+    })
+    name: string;
+}
