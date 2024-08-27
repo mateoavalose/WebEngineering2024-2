45b430a3daf97cf55c41c0e9dde0ef657e95f8e1
Mateo <mateo.avalos3942@gmail.com>
2024-08-27 14:05:07 -0500

    Changed swagger endpoint

--- TypeORM-Nest/src/main.ts
+++ TypeORM-Nest/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     })
   );
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('documentation-swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
