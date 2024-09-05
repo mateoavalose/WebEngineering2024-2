d44b2e8008bb62a310bfece1db4aa65f5eab9069
Mateo <mateo.avalos3942@gmail.com>
2024-09-05 14:35:00 -0500

    Added a provisional navbar

--- AndreaBusiness-Project/src/components/Menu/NavBar.jsx
+++ AndreaBusiness-Project/src/components/Menu/NavBar.jsx
@@ -1,9 +1,22 @@
-
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
     return (
         <>
-            <h1>NavBar</h1>
+            <nav className="bg-gray-50 dark:bg-gray-700">
+                <div className="max-w-screen-xl px-4 py-3 mx-auto">
+                    <div className="flex items-center">
+                        <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                            <li>
+                                <Link to="/home" className="text-gray-900 dark:text-white hover:underline">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="*" className="text-gray-900 dark:text-white hover:underline">404</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
