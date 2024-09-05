994363c909af260673a33aa25695c91453aebb80
Mateo <mateo.avalos3942@gmail.com>
2024-09-05 17:26:55 -0500

    Modified NavBar to handle routing. Deleted RouterR

--- AndreaBusiness-Project/src/App.jsx
+++ AndreaBusiness-Project/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
-import { RouterR } from './components/Menu/RouterR'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { NavBar } from './components/Menu/NavBar'
+import { RouteError } from './components/Menu/RouteError';
+
+// Components
+import Home from './components/HomePage/homePage';
+
 
 function App() {
   return (
-    <>
-      <RouterR/>
-    </>
-  )
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<RouteError />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App
