ac263a83c6eb5a421905d9f47d6d71ed758661b6
Mateo <mateo.avalos3942@gmail.com>
2024-09-06 11:38:29 -0500

    Fixed rows on error component

--- AndreaBusiness-Project/src/components/Menu/RouteError.jsx
+++ AndreaBusiness-Project/src/components/Menu/RouteError.jsx
@@ -1,9 +1,19 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 export const RouteError = () => {
     return (
-        <div>
-            <h1>404: Not Found</h1>
-            <h2>Sorry, the page you are looking for does not exist.</h2>
-            <img src="https://media1.tenor.com/m/lx2WSGRk8bcAAAAC/pulp-fiction-john-travolta.gif" alt="John Travolta is confused with the direction you want to go" />
-        </div>
+        <Container className="error">
+            <Col size={12}>
+            <Row>
+                <h1>404: Not Found</h1>
+            </Row>
+            <Row>
+                <h2>Sorry, the page you are looking for does not exist.</h2>
+            </Row>
+            <Row>
+                <img src="https://media1.tenor.com/m/lx2WSGRk8bcAAAAC/pulp-fiction-john-travolta.gif" alt="John Travolta is confused with the direction you want to go" />
+            </Row>
+            </Col>
+        </Container>
     )
 }
