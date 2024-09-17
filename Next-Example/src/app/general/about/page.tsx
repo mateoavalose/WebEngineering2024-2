7c08e7cc8025f6b37362497d5474d6acaf77f923
Mateo <mateo.avalos3942@gmail.com>
2024-09-17 06:37:56 -0500

    Added two new components

---
+++ Next-Example/src/app/general/about/page.tsx
@@ -0,0 +1,8 @@
+const aboutPage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center">About Page</h1>
+        </div>
+    );
+};
+export default aboutPage;
---
+++ Next-Example/src/app/general/contact/page.tsx
@@ -0,0 +1,8 @@
+const contactPage = () => {
+    return (
+        <div>
+        <h1 className="text-2xl font-bold text-center">Contact</h1>
+        </div>
+    );
+};
+export default contactPage;
